8a69b0588184049e70e118eefa462bda6c8c4f9d
ovaphlow <ovaphlow@outlook.com>
2018-05-14 22:51:22 +0800

    .

---
+++ webapp/src/user.resume.js
@@ -0,0 +1,11 @@
+import navbar from './navbar.html'
+document.getElementById('navbar').innerHTML = navbar
+
+console.log(1123)
+
+let app = new Vue({
+  el: '#app',
+  data: {},
+  methods: {},
+  created: function () {}
+})
