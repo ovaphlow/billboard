9c3a1812bcd5dcbe3846fcbbf1a1daf92e1e714d
ovaphlow <ovaphlow@live.com>
2018-06-22 15:14:46 +0800

    .

--- webapp/src/hypervisor/login.js
+++ webapp/src/hypervisor/login.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom'
 class Login extends React.Component {
   render() {
     return (
-      <div className="col-12">
+      <div className="col-3 offset-9">
         <div className="card">
           <div className="card-title">
             <h5>用户登录</h5>
