c91c497f0a450290a3503f61c4dea7ad4b0e0422
limeng6600 <229898977@qq.com>
2018-08-03 21:03:22 +0800

    .
    增加简历每日投递次数。限制次数为每日十次。
    增加页面投递按钮功能判断接口。每日一份简历只可以投递一家公司一次。

--- backend/routes/job.js
+++ backend/routes/job.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const log4js = require('log4js')
+var moment = require('moment')
 
 const config = require('../config')
 const sequelize = require('../util/sequelize')
@@ -67,28 +68,84 @@ router.get('/category/:category', (req, res) => {
  */
 router.route('/:job_uuid/user').post((req, res) => {
   req.body.job_uuid = req.params.job_uuid
-  let sql = `
-    insert into
-      ${config.database.schema}.post_resume
-    set
-      uuid = uuid(),
-      job_uuid = :job_uuid,
-      resume_uuid = (select uuid from ${config.database.schema}.resume where user_uuid = :user_uuid limit 1)
-  `
-  sequelize.query(sql, {
-    replacements: req.body,
-    type: sequelize.QueryTypes.INSERT
-  }).then(result => {
-    if (result[1] === 1) {
-      res.json({ content: '', message: '' })
-    } else {
-      res.json({ content: '', message: '投递简历失败。' })
+  
+  let date = moment(new Date()).format('YYYY-MM-DD');
+  let sql1 = `select count(*) count
+    from 
+    ${config.database.schema}.post_resume
+    where
+    date_format(date, '%Y-%m-%d') = :date
+    and resume_uuid = (select uuid from ${config.database.schema}.resume where user_uuid = :user_uuid limit 1)
+  ` 
+  sequelize.query(sql1, {
+    replacements:{
+      date: date,
+      user_uuid: req.body.user_uuid,
+      job_uuid: req.body.job_uuid
+    },
+    type: sequelize.QueryTypes.SELECT
+  }).then(result =>{
+    if(result[0].count <= 10){
+        let sql = `
+        insert into
+          ${config.database.schema}.post_resume
+        set
+          uuid = uuid(),
+          job_uuid = :job_uuid,
+          resume_uuid = (select uuid from ${config.database.schema}.resume where user_uuid = :user_uuid limit 1)
+      `
+      sequelize.query(sql, {
+        replacements: req.body,
+        type: sequelize.QueryTypes.INSERT
+      }).then(result => {
+        if (result[1] === 1) {
+          res.json({ content: '', message: '' })
+        } else {
+          res.json({ content: '', message: '投递简历失败。' })
+        }
+      }).catch(err => {
+        logger.error(err)
+        res.json({ content: '', message: '服务器错误。' })
+      })
+    }else{
+      res.json({ content: '500', message: '投递数量超过每日上限'})
     }
   }).catch(err => {
     logger.error(err)
-    res.json({ content: '', message: '服务器错误。' })
+    res.json({ content: '', message: '服务器错误。'})
   })
+ 
 })
+
+/**
+ * 判断是否可以点击投递按钮。
+ * 默认一天一份简历仅可以投递一家公司一次。
+ */
+
+ router.route('/judge').post((req, res) => {
+   let sql = `
+    select count(*) count
+    from
+      ${config.database.schema}.post_resume
+    where
+      job_uuid = :job_uuid
+      and resume_uuid = (select uuid from ${config.database.schema}.resume where user_uuid = :user_uuid limit 1)
+   `
+
+   sequelize.query(sql, {
+     replacements: req.body,
+     type: sequelize.QueryTypes.SELECT
+   }).then(result =>{
+     if(result[0].count <1){
+      res.json({content:'200', message:''})
+     }else{
+      res.json({content:'500', message:'请明天投递'})
+     }
+   }).catch(err =>{
+     logger.error(err)
+     res.json({content:'500', message:'服务器错误'})
+   })
+ })
 
 router.route('/:uuid').get((req, res) => {
   let sql = `
